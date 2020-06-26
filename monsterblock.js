5ad3687b5dcefd753ef50a472355171d92c89294
zeel <zeel02@gmail.com>
2020-06-26 02:19:29 -0400

    I guess you define the template differently than specified in the tutorial. You need a template property on the class that returns the path. Whatever the nonsense inside of defaultOptions is, it doesn't define the template.

--- monsterblock.js
+++ monsterblock.js
@@ -1,11 +1,13 @@
 import ActorSheet5eNPC from "../../systems/dnd5e/module/actor/sheets/npc.js";
 
 export class MonsterBlock5e extends ActorSheet5eNPC {
-
+	get template() {
+		return "modules/monsterblock/actor-sheet.html";
+	}
 	static get defaultOptions() {
 		return mergeObject(super.defaultOptions, {
 			classes: ["monsterblock", "sheet", "actor"],
-			template: "modules/monsterblock/actor-sheet.html",
+		//	template: "modules/monsterblock/actor-sheet.html",
 			width: 600,
 			height: 600,
 			tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
