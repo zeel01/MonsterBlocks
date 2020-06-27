7a690cbd7a68b8499b22fc2f49ace94f2b32c90b
zeel <zeel02@gmail.com>
2020-06-27 02:16:18 -0400

    Added attacks and features, working on other abilities.
    Finding the parsed "chatData" text for description with inline rolls was a bit annoying.

--- monsterblock.js
+++ monsterblock.js
@@ -8,8 +8,8 @@ export class MonsterBlock5e extends ActorSheet5eNPC {
 		return mergeObject(super.defaultOptions, {
 			classes: ["monsterblock", "sheet", "actor"],
 		//	template: "modules/monsterblock/actor-sheet.html",
-			width: 900,
-			height: 900,
+			width: 800,
+			height: 800,
 			tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
 		});
 	}
@@ -49,4 +49,34 @@ Handlebars.registerHelper("hassave", (saves)=> {
 		if (saves[s].proficient) return true;
 	}
 	return false;
+});
+Handlebars.registerHelper("getattacks", (features)=> {
+	for (let f in features) {
+		if (features[f].label == "Attacks") return features[f].items;
+	}
+});
+Handlebars.registerHelper("getactions", (features)=> {
+	for (let f in features) {
+		if (features[f].label == "Actions") return features[f].items;
+	}
+});
+Handlebars.registerHelper("getfeatures", (features)=> {
+	for (let f in features) {
+		if (features[f].label == "Features") return features[f].items;
+	}
+});
+Handlebars.registerHelper("getattacktype", (attack)=> {
+	return "DND5E.Action" + attack.data.actionType.toUpperCase();
+});
+Handlebars.registerHelper("getattackbonus", (attack, data)=> {
+	let attr = attack.data.ability;
+	let abilityBonus = data.abilities[attr].mod;
+	let isProf = attack.data.proficient;
+	let profBonus = data.attributes.prof;
+//	console.log(attr, abilityBonus, isProf, profBonus);
+	
+	return abilityBonus + (isProf ? profBonus : 0);
+});
+Handlebars.registerHelper("getchathtml", (item, actor)=> {
+	return game.actors.get(actor._id).getOwnedItem(item._id).getChatData().description.value;
 });
