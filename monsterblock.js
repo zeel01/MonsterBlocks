027e0c847f8026eac7f277e835172cd6279a8357
zeel <zeel02@gmail.com>
2020-06-29 22:57:59 -0400

    Refactored a bit because my helpers should really be scoped inside the class so they can access `this`, and I', working on adding a control bar to toggle settings.

--- monsterblock.js
+++ monsterblock.js
@@ -1,6 +1,12 @@
 import ActorSheet5eNPC from "../../systems/dnd5e/module/actor/sheets/npc.js";
 
 export class MonsterBlock5e extends ActorSheet5eNPC {
+	constructor(...args) {
+		super(...args);
+		
+		this.createHandlebarsHelpers();
+	}
+
 	get template() {
 		return "modules/monsterblock/actor-sheet.html";
 	}
@@ -14,23 +20,140 @@ export class MonsterBlock5e extends ActorSheet5eNPC {
 		});
 	}
 	
-	 activateListeners(html) {
-		 //
-	 }
+	getData() {
+		const data = super.getData();
+		data.flags = this.actor.data.flags.monsterblock;
+		return data;
+	}
+	
+	activateListeners(html) {
+		html.find('.switch').click(this._onSwitch.bind(this));
+	}
+	
+	toggleAttackDescription() {
+		let flag = !this.actor.getFlag("monsterblock", "attack-descriptions");
+		console.log(flag);
+		this.actor.setFlag("monsterblock", "attack-descriptions", flag);
+	}
+	
+	_onSwitch(event) {
+		console.log(event);
+		let control = event.target.dataset.control;
+		switch (control) {
+			case "attack-description": this.toggleAttackDescription(); break;
+			default: console.warn(`Monster Block | ${control} is not a valid switch.`);
+		}
+	}
+	isMultiAttack(item) {
+		let name = item.name.toLowerCase().replace(/\s+/g, '');
+		return [
+			"multiattack",
+			"extraattack",
+			"extraattacks",
+			"multiattacks",
+			"multipleattacks",
+			"manyattacks"
+		].includes(name);
+	}
+	isLegendaryAction(item) {
+		return item.data.activation.type === "legendary";
+	}
+	isLairAction(item) {
+		return item.data.activation.type === "lair";
+	}
+	createHandlebarsHelpers() {
+		Handlebars.registerHelper("hascontents", (obj)=> {
+			return Object.keys(obj).length > 0;
+		});
+
+		Handlebars.registerHelper("hasskills", (skills)=> {
+			for (let s in skills) {
+				if (skills[s].value) return true;
+			}
+			return false;
+		});
+		Handlebars.registerHelper("hassave", (saves)=> {
+			for (let s in saves) {
+				if (saves[s].proficient) return true;
+			}
+			return false;
+		});
+		
+		Handlebars.registerHelper("haslegendary", (features)=> {
+			for (let feature of features) {
+				if (feature.label == "Actions") {
+					let items = feature.items;
+					for (let item of items) {
+						if (item.data.activation.type === "legendary") return true;
+					}
+				}
+			}
+			return false;
+		});
+		Handlebars.registerHelper("islegendary", (item)=> {
+			return this.isLegendaryAction(item);
+		});
+		Handlebars.registerHelper("getmultiattack", (features)=> {
+			for (let feature of features) {
+				let items = feature.items;
+				for (let item of items) {
+					if (this.isMultiAttack(item)) return item;
+				}
+			}
+			return false;
+		});
+		Handlebars.registerHelper("notspecialaction", (item)=> {
+			// Handlebars has no negation in conditions afik, so we have to create one.
+			return !(this.isMultiAttack(item) || this.isLegendaryAction(item) || this.isLairAction(item));
+		});
+		Handlebars.registerHelper("getattacks", (features)=> {
+			for (let feature of features) {
+				if (feature.label == "Attacks") return feature.items;
+			}
+		});
+		Handlebars.registerHelper("getactions", (features)=> {
+			for (let feature of features) {
+				if (feature.label == "Actions") return feature.items;
+			}
+		});
+		Handlebars.registerHelper("getfeatures", (features)=> {
+			for (let feature of features) {
+				if (feature.label == "Features") return feature.items;
+			}
+		});
+		Handlebars.registerHelper("getattacktype", (attack)=> {
+			return "DND5E.Action" + attack.data.actionType.toUpperCase();
+		});
+		Handlebars.registerHelper("getattackbonus", (attack, data)=> {
+			let attr = attack.data.ability;
+			let abilityBonus = data.abilities[attr].mod;
+			let isProf = attack.data.proficient;
+			let profBonus = data.attributes.prof;
+		//	console.debug(attr, abilityBonus, isProf, profBonus);
+			
+			return abilityBonus + (isProf ? profBonus : 0);
+		});
+		Handlebars.registerHelper("getchathtml", (item, actor)=> {
+			return game.actors.get(actor._id).getOwnedItem(item._id).getChatData().description.value;
+		});
+		Handlebars.registerHelper("enrichhtml", (str)=> {
+			return TextEditor.enrichHTML(str, {secrets: true});
+		});		
+	}
 }
 
 Hooks.on("init", () => {
-	console.log(`Monster Block | Initialized.`);
+	console.log(`Monster Block | %cInitialized.`, "color: orange");
 });
 
 Hooks.on("renderActorSheet", ()=> {
 	let template = "modules/monsterblock/actor-sheet.html";
     delete _templateCache[template];
-    console.log(`Monster Block | removed "${template}" from _templateCache.`);
+    console.debug(`Monster Block | removed "${template}" from _templateCache.`);
 })
 
 Hooks.on("renderMonsterBlock5e", (monsterblock, html, data)=> {
-	console.log(`Monster Block |`, monsterblock, html, data);
+	console.debug(`Monster Block |`, monsterblock, html, data);
 	
 	let popup = monsterblock._element[0];
 	let anchorPosL = popup.querySelector("#endAnchor").offsetLeft;
@@ -46,7 +169,7 @@ Hooks.on("renderMonsterBlock5e", (monsterblock, html, data)=> {
 		let shrink = (h - anchorPosT) / 2;
 		let nh = h - shrink;
 		if (anchorPosL < w) nh = anchorPosT;
-		popup.style.height = nh + 12 + "px";
+		popup.style.height = nh + 38 + "px";
 	}
 });
 
@@ -55,96 +178,3 @@ Actors.registerSheet("dnd5e", MonsterBlock5e, {
     makeDefault: false
 });
 
-Handlebars.registerHelper("hascontents", (obj)=> {
-	return Object.keys(obj).length > 0;
-});
-
-Handlebars.registerHelper("hasskills", (skills)=> {
-	for (let s in skills) {
-		if (skills[s].value) return true;
-	}
-	return false;
-});
-Handlebars.registerHelper("hassave", (saves)=> {
-	for (let s in saves) {
-		if (saves[s].proficient) return true;
-	}
-	return false;
-});
-function isMultiAttack(item) {
-	let name = item.name.toLowerCase().replace(/\s+/g, '');
-	return [
-		"multiattack",
-		"extraattack",
-		"extraattacks",
-		"multiattacks",
-		"multipleattacks",
-		"manyattacks"
-	].includes(name);
-}
-function isLegendaryAction(item) {
-	return item.data.activation.type === "legendary";
-}
-function isLairAction(item) {
-	return item.data.activation.type === "lair";
-}
-Handlebars.registerHelper("haslegendary", (features)=> {
-	for (let feature of features) {
-		if (feature.label == "Actions") {
-			let items = feature.items;
-			for (let item of items) {
-				if (item.data.activation.type === "legendary") return true;
-			}
-		}
-	}
-	return false;
-});
-Handlebars.registerHelper("islegendary", (item)=> {
-	return isLegendaryAction(item);
-});
-Handlebars.registerHelper("getmultiattack", (features)=> {
-	for (let feature of features) {
-		let items = feature.items;
-		for (let item of items) {
-			if (isMultiAttack(item)) return item;
-		}
-	}
-	return false;
-});
-Handlebars.registerHelper("notspecialaction", (item)=> {
-	// Handlebars has no negation in conditions afik, so we have to create one.
-	return !(isMultiAttack(item) || isLegendaryAction(item) || isLairAction(item));
-});
-Handlebars.registerHelper("getattacks", (features)=> {
-	for (let feature of features) {
-		if (feature.label == "Attacks") return feature.items;
-	}
-});
-Handlebars.registerHelper("getactions", (features)=> {
-	for (let feature of features) {
-		if (feature.label == "Actions") return feature.items;
-	}
-});
-Handlebars.registerHelper("getfeatures", (features)=> {
-	for (let feature of features) {
-		if (feature.label == "Features") return feature.items;
-	}
-});
-Handlebars.registerHelper("getattacktype", (attack)=> {
-	return "DND5E.Action" + attack.data.actionType.toUpperCase();
-});
-Handlebars.registerHelper("getattackbonus", (attack, data)=> {
-	let attr = attack.data.ability;
-	let abilityBonus = data.abilities[attr].mod;
-	let isProf = attack.data.proficient;
-	let profBonus = data.attributes.prof;
-//	console.log(attr, abilityBonus, isProf, profBonus);
-	
-	return abilityBonus + (isProf ? profBonus : 0);
-});
-Handlebars.registerHelper("getchathtml", (item, actor)=> {
-	return game.actors.get(actor._id).getOwnedItem(item._id).getChatData().description.value;
-});
-Handlebars.registerHelper("enrichhtml", (str)=> {
-	return TextEditor.enrichHTML(str, {secrets: true});
-});
