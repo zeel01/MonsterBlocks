c123b71bf21bbcfc97ad9275fb7d2dfbf8ca37ba
zeel <zeel02@gmail.com>
2020-08-03 16:18:15 -0400

    Cleaning up code

--- monsterblock.js
+++ monsterblock.js
@@ -1,7 +1,4 @@
 import ActorSheet5eNPC from "../../systems/dnd5e/module/actor/sheets/npc.js";
-import Tokenizer from "../vtta-tokenizer/src/tokenizer/index.js";
-
-// A totally different comment
 
 export class MonsterBlock5e extends ActorSheet5eNPC {
 	constructor(...args) {
