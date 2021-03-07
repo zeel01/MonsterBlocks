454cc27dba5ce7ca7a0c14920bf7a416b8f7cd79
zeel <zeel02@gmail.com>
2021-03-07 16:46:19 -0500

    Moved attributes1 to partial

--- scripts/dnd5e/MonsterBlock5e.js
+++ scripts/dnd5e/MonsterBlock5e.js
@@ -1711,12 +1711,13 @@ export default class MonsterBlock5e extends ActorSheet5eNPC {
 			
 			// Actor Sheet Partials
 			"modules/monsterblock/templates/dnd5e/parts/identity.hbs",
+			"modules/monsterblock/templates/dnd5e/parts/attributes1.hbs",
 
 			"modules/monsterblock/templates/dnd5e/parts/spellcasting.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/attack.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/legendaryActs.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/lairActs.hbs",
-			
+
 			"modules/monsterblock/templates/dnd5e/parts/menuItem.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/resource.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/featureBlock.hbs",
