6fc30a6245b6afa597d9d3089ecafb831091fc5a
zeel <zeel02@gmail.com>
2021-03-07 17:14:23 -0500

    Moved skills to partial

--- scripts/dnd5e/MonsterBlock5e.js
+++ scripts/dnd5e/MonsterBlock5e.js
@@ -1715,6 +1715,7 @@ export default class MonsterBlock5e extends ActorSheet5eNPC {
 			"modules/monsterblock/templates/dnd5e/parts/header/abilities.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/header/attributes2.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/header/saves.hbs",
+			"modules/monsterblock/templates/dnd5e/parts/header/skills.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/header/damage.hbs",
 
 			"modules/monsterblock/templates/dnd5e/parts/main/spellcasting.hbs",
