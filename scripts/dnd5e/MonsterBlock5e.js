a5d8d7bcd54d1cf170cdc0fdd0a3454563174fc1
zeel <zeel02@gmail.com>
2021-03-07 17:19:16 -0500

    Moved senses to partial

--- scripts/dnd5e/MonsterBlock5e.js
+++ scripts/dnd5e/MonsterBlock5e.js
@@ -1716,6 +1716,7 @@ export default class MonsterBlock5e extends ActorSheet5eNPC {
 			"modules/monsterblock/templates/dnd5e/parts/header/attributes2.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/header/saves.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/header/skills.hbs",
+			"modules/monsterblock/templates/dnd5e/parts/header/senses.hbs",
 			"modules/monsterblock/templates/dnd5e/parts/header/damage.hbs",
 
 			"modules/monsterblock/templates/dnd5e/parts/main/spellcasting.hbs",
