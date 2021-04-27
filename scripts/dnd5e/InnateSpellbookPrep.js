e16550718af6840b8df96c6475b7091196d7c069
zeel <zeel02@gmail.com>
2021-04-27 12:15:37 -0400

    Refactored innate spellbook

---
+++ scripts/dnd5e/InnateSpellbookPrep.js
@@ -0,0 +1,136 @@
+import ResourcePreper from "./ResourcePreper.js";
+
+export default class InnateSpellbookPrep {
+	/**
+	 * Creates an instance of InnateSpellbookPrep.
+	 *
+	 * @param {object} spellbook                            - The standard spellbook for the actor
+	 * @param {import("./MonsterBlock5e.js").default} sheet - The Monster Block instance
+	 * @memberof InnateSpellbookPrep
+	 */
+	constructor(spellbook, sheet) {
+		this.spellbook = spellbook;
+		this.sheet = sheet;
+		this.book = [];
+	}
+
+	/**
+	 * Find all the innate spells, prepare their resources, and sort them into pages by max uses.
+	 *
+	 * @return {object} The completed innate spellbook 
+	 * @memberof InnateSpellbookPrep
+	 */
+	prepare() {                                              // We need to completely re-organize the spellbook for an innate spellcaster
+		for (let level of this.spellbook) {                  // Spellbook is seperated into sections based on level, though all the innate spells are lumped together, we still want to check all the sections.
+			if (level.prop !== "innate") continue;           // We don't care about sections that aren't marked as innate though
+			this.prepareSpellLevel(level);
+		}
+
+		this.book.sort(this.constructor.pageSort);
+
+		return this.book;
+	}
+
+	/**
+	 * For a given level of spells, prepare those spell respurces and sort
+	 * them into the book.
+	 *
+	 * @param {object} level - All spells of a given level from the original book
+	 * @memberof InnateSpellbookPrep
+	 */
+	prepareSpellLevel(level) {
+		for (let spell of level.spells) {
+			this.prepSpellResource(spell);
+			this.sortSpell(spell);
+		}
+	}
+
+	/**
+	 * Prepares the resource of a spell
+	 *
+	 * @param {object} spellData
+	 * @memberof InnateSpellbookPrep
+	 */
+	prepSpellResource(spellData) {
+		const spell = this.sheet.object.items.get(spellData._id);
+		spellData.hasresource = ResourcePreper.hasResource(spell);
+		if (!spellData.hasresource) return;
+		
+		spellData.resource = new ResourcePreper(
+			spellData, spell, this.sheet
+		).getResource();
+	}
+
+	/**
+	 * Sorts the spell into the appropriate page of the new book.
+	 *
+	 * @param {object} spell
+	 * @memberof InnateSpellbookPrep
+	 */
+	sortSpell(spell) {
+		// Max uses is what we are going to end up sorting the spellbook by.
+		this.getPage(spell.data.uses.max).spells.push(spell);
+	}
+
+	/**
+	 * Finds or creates a page in the new spellbook for spells with a 
+	 * certain number of maximum uses.
+	 *
+	 * @param {number} uses - The maximum number of uses that all spells on this page have
+	 * @return {object}       The spellbook page 
+	 * @memberof InnateSpellbookPrep
+	 */
+	getPage(uses) {
+		return this.book.find(page => page.uses == uses) // Find the page
+			|| this.addPage(uses);                       // Or add a new one
+	}
+	/**
+	 * Creates a new empty page for the spellbook.
+	 *
+	 * Most of this is just intended to match the 
+	 * data in the regular spell book, though most 
+	 * isn't ultimately going to be used.
+	 *
+	 * @param {number} uses - The maximum number of uses that all spells on this page have
+	 * @return {object}       The spellbook page
+	 * @memberof InnateSpellbookPrep
+	 */
+	addPage(uses) {
+		const page = {
+			canCreate: false,
+			canPrepare: false,
+			dataset: { level: -10, type: "spell" },
+			// This is important, as this string will be used to display on the sheet.
+			label: uses < 1 ? "At will" : (uses + "/day"),
+			order: -10,
+			override: 0,
+			prop: "innate",
+			slots: "-",
+			spells: [],      // An empty array to put spells in later.
+			uses: uses,      // How we will identify this type of spell later, rather than by spell level.
+			usesSlots: false
+		}
+		this.book.push(page);
+		return page;
+	}
+
+	/**
+	 * A sorting function for {@link Array.prototype.sort}
+	 *
+	 * Orders pages a and b descending by number of uses,
+	 * but sorts a page with 0 (unlimited) uses to the start.
+	 *
+	 * @static
+	 * @param {object} a - A spellbook page
+	 * @param {object} b - A spellbook page
+	 * @return {number}    The order to sort the twoo pages @see Array.prototype.sort
+	 * @memberof InnateSpellbookPrep
+	 */
+	static pageSort(a, b) {
+		if (a.uses == 0 && b.uses == 0) return 0;
+		if (a.uses == 0) return -1;
+		if (b.uses == 0) return 1;
+
+		return a.uses < b.uses ? 1 : -1;
+	}
+}
--- scripts/dnd5e/ItemPrep.js
+++ scripts/dnd5e/ItemPrep.js
@@ -1,9 +1,9 @@
 import MonsterBlock5e from "./MonsterBlock5e.js";
 import AttackPreper from "./AttackPreper.js";
 import CastingPreper from "./CastingPreper.js";
-import ResourcePreper from "./ResourcePreper.js";
 import ActionPreper from "./ActionPreper.js";
 import ItemPreper from "./ItemPreper.js";
+import InnateSpellbookPrep from "./InnateSpellbookPrep.js"
 
 /**
  * @typedef {import{"../../../../systems/dnd5e/module/item/sheet.js"}.Item5e} Item5e
@@ -57,7 +57,7 @@ export default class ItemPrep {
 
 		// Organize Spellbook
 		data.spellbook = this.sheet._prepareSpellbook(data, spells);
-		data.innateSpellbook = this.prepareInnateSpellbook(data.spellbook); 
+		data.innateSpellbook = new InnateSpellbookPrep(data.spellbook, this.sheet).prepare(); //this.prepareInnateSpellbook(data.spellbook);
 
 		// Organize Features
 		for ( let item of other ) {
@@ -70,47 +70,6 @@ export default class ItemPrep {
 
 		// Assign and return
 		data.features = features;
-	}
-
-	prepareInnateSpellbook(spellbook) { // We need to completely re-organize the spellbook for an innate spellcaster
-		let innateSpellbook = [];
-
-		for (let level of spellbook) {								// Spellbook is seperated into sections based on level, though all the innate spells are lumped together, we still want to check all the sections.
-			if (level.prop !== "innate") continue;					// We don't care about sections that aren't marked as innate though
-			for (let spell of level.spells) {						// Check all the spells
-				let uses = spell.data.uses.max;						// The max uses are the only thing actually displayed, though the data tracks usage
-																	// Max uses is what we are going to end up sorting the spellbook by.
-				let finder = e => e.uses == uses;					// The conditional expression for later. We are going to check if our new spellbook has a section for this spells usage amount.
-				
-				if (!innateSpellbook.some(finder)) {				// Array.some() is used to check the whole array to see if the condition is ever satisfied.
-					innateSpellbook.push({							// If there isn't a section to put this spell into, we create a new one.
-						canCreate: false,							// Most of this is just intended to match the data in the regular spell book, though most isn't ultimately going to be used.
-						canPrepare: false,
-						dataset: { level: -10, type: "spell" },
-						label: uses < 1 ? "At will" : (uses + "/day"),	// This is important, as this string will be used to display on the sheet.
-						order: -10,
-						override: 0,
-						prop: "innate",
-						slots: "-",
-						spells: [],									// An empty array to put spells in later.
-						uses: uses,									// How we will identify this type of spell later, rather than by spell level.
-						usesSlots: false
-					});
-				}
-				
-				spell.resource = new ResourcePreper(spell, this.sheet.object.items.get(spell._id), this.sheet).getResource();
-				innateSpellbook.find(finder).spells.push(spell);	// We can use the same condition as above, this time to lacate the item that satisfies the condition. We then insert the current spell into that section.
-			}
-		}
-		innateSpellbook.sort((a, b) => {	// This sorts the spellbook sections, so that the first section is the "0" useage one, which is actually infinite uses - At will, and Cantrips.
-			if (a.uses == 0 && b.uses == 0) return 0;
-			if (a.uses == 0) return -1;
-			if (b.uses == 0) return 1;
-			
-			return a.uses < b.uses ? 1 : -1;
-		});
-		
-		return innateSpellbook;
 	}
 
 	/**
